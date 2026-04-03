f44323ba2e51764729f145b7b823f9f0c24b0158
work-7221 <work.rohankm7221@gmail.com>
2026-04-03 21:00:35 +0530

    Started with JS!

---
+++ Video 54/script.js
@@ -0,0 +1,22 @@
+
+alert("Hello World");
+
+console.log("Code is running..")
+console.log("Code is running smoothly..")
+console.log("Code is now at the end..")
+
+var a = prompt (
+    "enter your number "
+)
+
+console.log(a)
+
+var isTrue = confirm (
+    "hello, wanna sell your kidneys?"
+)
+
+if (isTrue) {
+    console.log("Sold your kidneys to the darkweb!")
+} else {
+    console.log("we need them kidneys, please contact us later!")
+}
