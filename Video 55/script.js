7063cd30da4982d25cd2714520d9f2b8f259585b
work-7221 <work.rohankm7221@gmail.com>
2026-04-04 09:12:36 +0530

    Learnt about JS variables and datatypes!

---
+++ Video 55/script.js
@@ -0,0 +1,43 @@
+console.log("Code is running!")
+console.log("This is a JS tutorial!")
+
+
+var a = 5
+
+var b = 6
+
+var c = "Rohan"
+
+console.log(a+b+c)
+
+console.log(typeof a, typeof b, typeof c)   
+
+
+
+const a1 = 69;
+// a1 = a1 + 9;
+
+
+console.log("This is an example of local and global scope!")
+var f = 5;
+console.log(f)
+
+{
+    let f = 9;
+    console.log(f);
+}
+
+
+
+
+
+let x = "Harry bhai"
+let y = 22;
+let z = 3.55
+
+const p = true
+let q = undefined
+let r = null;
+
+console.log(x, y, z, p, q, r)
+console.log(typeof x, typeof y, typeof z, typeof p, typeof q, typeof r)
