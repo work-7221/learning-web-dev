ab2601ccad81ad20a1802f46c9a14b0493dac956
work-7221 <work.rohankm7221@gmail.com>
2026-04-04 10:58:33 +0530

    Learnt about JS conditional statments and if-else ladder!

---
+++ video 56/script.js
@@ -0,0 +1,18 @@
+console.log("Hello i am conditional tutorial!")
+
+let age = 17
+let grace = 2
+
+console.log(age += grace)
+console.log(age -= grace)
+console.log(age *= grace)
+console.log(age /= grace)
+console.log(age **= grace)
+// if (age+grace > 18) {
+//     console.log("You can drive!")
+// } else {
+//     console.log("You can't drive!")
+// }
+
+console.log(age)
+
