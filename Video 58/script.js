d9079e7ddc0875dfcd855a23aefc0d5e6b94d0c8
work-7221 <work.rohankm7221@gmail.com>
2026-04-06 16:11:31 +0530

    Learnt about functions in JS and also about arrow functions!

---
+++ Video 58/script.js
@@ -0,0 +1,38 @@
+console.log("Code is running")
+
+
+// functions in JS
+
+
+function greet(name) {
+    console.log("Hey " + name + ", how are you?")
+    console.log("You seem to be a nice person ")
+    console.log("Hey " + name + " where do you work?")
+}
+
+greet("Rohan")
+
+
+function summing(a, b) {
+    return (a + b)
+}
+c = summing(7, 7)
+
+console.log(c)
+
+
+const func1 = (x)=> {
+    console.log("I am an arrow function", x)
+}
+
+func1(90)
+func1(91)
+func1(92)
+func1(93)
+func1(94)
+func1(95)
+func1(96)
+func1(97)
+func1(98)
+func1(99)
+func1(100)
