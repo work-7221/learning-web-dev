19968af6794d6e5c2508d42e729981567117a6a4
work-7221 <work.rohankm7221@gmail.com>
2026-04-13 11:35:58 +0530

    Learnt about various methods to access nodes, children nodes, elements, sibling elements!

---
+++ Video 67 JavaScript DOM - Children, Parent & Sibling Nodes/script.js
@@ -0,0 +1,25 @@
+console.log("Code is running")
+
+// console.log(document.body.childNodes)
+
+// console.log("Phase 1")
+// console.log(document.body.childNodes[0])
+// console.log(document.body.childNodes[1])
+// console.log(document.body.childNodes[2])
+// console.log(document.body.childNodes[3].childNodes)
+
+
+// console.log("Phase 2")
+// console.log(document.body.firstChild)
+// console.log(document.body.lastChild)
+
+console.log("Phase 3")
+
+// let cont = document.body.childNodes[1];
+// console.log(document.body.childNodes[1])
+// console.log(cont.firstElementChild)
+console.log(document.body.firstElementChild)
+console.log(document.body.firstElementChild.firstElementChild)
+console.log(document.body.firstElementChild.children[3].previousSibling)
+console.log(document.body.firstElementChild.children[3].previousElementSibling)
+console.log(document.body.firstElementChild.children[3])
